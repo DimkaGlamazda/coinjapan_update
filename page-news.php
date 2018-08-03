2ddbce176685f991a5d07ebd2c9ae8bc111044ce
dima <gldmvl@gmail.com>
2018-08-03 19:19:30 +0300

    Updated news list view styles

--- page-news.php
+++ page-news.php
@@ -88,22 +88,22 @@ $latest_news = null;
 $news = [];
 
 
-
 while ($news_data->have_posts()) :
 
   $news_data->the_post();
 
   $categories_data = wp_get_object_terms($post->ID, 'categories');
+
+
+
   $news_item['categories'] = [];
-  foreach ($categories_data as $category)
-  {
+  foreach ($categories_data as $category) {
     $news_item['categories'][] = [
-      'title' => $category->name,
-      'color' => get_option("taxonomy_term_" . $category->term_id)['categories_id'],
-      'url' => get_permalink(get_page_by_path('news')) . '?'.$category->slug . '=' . $category->name
+        'title' => $category->name,
+        'color' => get_option("taxonomy_term_" . $category->term_id)['categories_id'],
+        'url' => get_permalink(get_page_by_path('news')) . '?' . $category->slug . '=' . $category->name
     ];
   }
-
 
   $custom_fields = get_post_custom(get_the_ID());
   $news_item['summary'] = $custom_fields['summary'][0];
@@ -111,15 +111,16 @@ while ($news_data->have_posts()) :
   $news_item['date'] = get_the_date('Y/n/j');
   $news_item['thumbnail'] = get_the_post_thumbnail_url();
   $news_item['title'] = get_the_title();
+  $news_item['link'] = get_permalink();
 
-  if($counter == 0)
-  {
+  if ($counter == 0) {
     $latest_news = $news_item;
+    $counter++;
     continue;
   }
 
   $news[] = $news_item;
-
+  $counter++;
 endwhile;
 
 
@@ -130,33 +131,55 @@ endwhile;
       <div class="news-container">
         <div class="row no-gutters">
           <div class="col-12">
-            <div class="latest-news-img-wrapper">
-              <img src="<?=$latest_news['thumbnail']?>" alt="<?=$latest_news['title']?>">
-            </div>
-            <div class="latest-news-content">
-              <h4 class="latest-news-header"><?=$latest_news['title']?></h4>
-              <?=$latest_news['summary']?>
-              <div class="news-item-footer">
-                <span class="news-item-footer-date"><?=$latest_news['date']?></span>
-                <div class="news-item-categories-list">
-                  <?php foreach($latest_news['categories'] as $category): ?>
-
-                    <a href="<?=$category['url']?>" class="news-item-footer-category" style="color:#<?=$category['color']?>;">
-                      <span class="news-item-footer-category-label" style="background: #<?=$category['color']?>"></span>
-                      <?=$category['title']?>
-                    </a>
-                  <?php endforeach; ?>
-                </div>
-
+            <a class="news-list-link-wrapper" href="<?=$latest_news['link']?>">
+              <div class="latest-news-img-wrapper">
+                <img src="<?=$latest_news['thumbnail']?>" alt="<?=$latest_news['title']?>">
               </div>
-            </div>
+              <div class="latest-news-content">
+                <h4 class="latest-news-header"><?=$latest_news['title']?></h4>
+                <?=$latest_news['summary']?>
+                <div class="news-item-footer">
+                  <span class="news-item-footer-date"><?=$latest_news['date']?></span>
+                  <div class="news-item-categories-list">
+                    <?php foreach ($latest_news['categories'] as $category): ?>
+                      <a href="<?=$category['url']?>" class="news-item-footer-category" style="color:#<?=$category['color']?>;">
+                        <span class="news-item-footer-category-label" style="background: #<?=$category['color']?>"></span>
+                        <?=$category['title']?>
+                      </a>
+                    <?php endforeach; ?>
+                  </div>
+                </div>
+              </div>
+            </a>
           </div>
         </div>
         <div class="news-list-other-news">
           <div class="row no-gutters">
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-
-            </div>
+            <?php foreach ($news as $piece_of_news): ?>
+              <div class="col-12 col-sm-6 col-md-6 col-lg-3">
+                <a class="news-list-link-wrapper" href="<?=$piece_of_news['link']?>">
+                  <div class="news-item-list-img">
+                    <img src="<?=$piece_of_news['thumbnail']?>" alt="<?=$piece_of_news['title']?>">
+                  </div>
+                  <div class="news-item-list-content">
+                    <h4><?=$piece_of_news['title']?></h4>
+                    <div class="news-item-footer">
+                      <span class="news-item-footer-date"><?=$latest_news['date']?></span>
+                      <div class="news-item-categories-list">
+                        <?php foreach ($piece_of_news['categories'] as $category): ?>
+                          <a href="<?=$category['url']?>" class="news-item-footer-category"
+                             style="color:#<?=$category['color']?>;">
+                          <span class="news-item-footer-category-label"
+                                style="background: #<?=$category['color']?>"></span>
+                            <?=$category['title']?>
+                          </a>
+                        <?php endforeach; ?>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            <?php endforeach; ?>
           </div>
         </div>
       </div>
